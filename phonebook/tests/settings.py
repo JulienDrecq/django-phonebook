e4eb15401d4a70e6fcc784fcc903e90d805ccdba
Julien DRECQ <drecq.julien@gmail.com>
2015-06-25 09:27:58 +0200

    [IMP] Add settings & urls for tests

---
+++ phonebook/tests/settings.py
@@ -0,0 +1,35 @@
+# Django settings for phonebook.
+SECRET_KEY = 'test_phonebook'
+DEBUG = True
+TEMPLATE_DEBUG = True
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'phonebook',
+)
+ROOT_URLCONF = 'phonebook.tests.urls'
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+)
+WSGI_APPLICATION = 'DjangoPhonebook.wsgi.application'
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+STATIC_URL = '/static/'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
